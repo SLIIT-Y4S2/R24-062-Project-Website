e78d62dce3096ef244d2f907dd026578ac5caed2
it21109126 <it21109126@my.sliit.lk>
2024-10-21 20:30:05 +0530

    Implemented client side email sending

--- src/app/components/GetInTouch.js
+++ src/app/components/GetInTouch.js
@@ -2,9 +2,42 @@
 import React from "react";
 import Link from "next/link";
 import * as Unicons from '@iconscout/react-unicons'
+import emailjs from '@emailjs/browser';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const handleSubmit = async ({name, email, subject, message}) => {
+    const params = {
+        from_name: name,
+        from_email: email,
+        to_email: "soloscholar.ai@gmail.com",
+        subject: subject,
+        message: message
+    }
+
+    try{
+        const response = await emailjs.send(
+            process.env.NEXT_PUBLIC_EMAIL_SERVICE_ID, 
+            process.env.NEXT_PUBLIC_EMAIL_TEMPLATE_ID, 
+            params, 
+            {
+                publicKey: process.env.NEXT_PUBLIC_EMAIL_PUBLIC_KEY
+            });
+
+        response.status === 200 ? alert("SUCCESS: Message sent successfully!") : alert("FAILED: Failed to send message!");
+    } catch(error){
+        alert('FAILED: ', error.message);
+    }
+}
 
 
 export default function GetInTouch(){
+    const[name, setName] = React.useState("");
+    const[email, setEmail] = React.useState("");
+    const[subject, setSubject] = React.useState("");
+    const[message, setMessage] = React.useState("");
+
     return(
         <section className="relative md:py-24 py-16 bg-gray-50 dark:bg-slate-800" id="contact">
         <div className="container">
@@ -17,24 +50,27 @@ export default function GetInTouch(){
             <div className="grid grid-cols-1 lg:grid-cols-12 md:grid-cols-2 mt-8 items-center gap-[30px]">
                 <div className="lg:col-span-8">
                     <div className="p-6 rounded-md shadow bg-white dark:bg-slate-900">
-                        <form>
+                        <form onSubmit={(e) => {
+                            e.preventDefault();
+                            handleSubmit({name, email, subject, message});
+                        }}>
                             <div className="grid lg:grid-cols-12 lg:gap-5">
                                 <div className="lg:col-span-6 mb-5">
-                                    <input name="name" id="name" type="text" className="form-input w-full py-2 px-3 border border-inherit dark:border-gray-800 dark:bg-slate-900 dark:text-slate-200 rounded h-10 outline-none bg-transparent focus:border-amber-500/50 dark:focus:border-amber-500/50 focus:shadow-none focus:ring-0 text-[15px]" placeholder="Name :"/>
+                                    <input name="name" id="name" type="text" className="form-input w-full py-2 px-3 border border-inherit dark:border-gray-800 dark:bg-slate-900 dark:text-slate-200 rounded h-10 outline-none bg-transparent focus:border-amber-500/50 dark:focus:border-amber-500/50 focus:shadow-none focus:ring-0 text-[15px]" placeholder="Name :" onChange={(e) => setName(e.target.value)} value={name} required/>
                                 </div>
 
                                 <div className="lg:col-span-6 mb-5">
-                                    <input name="email" id="email" type="email" className="form-input w-full py-2 px-3 border border-inherit dark:border-gray-800 dark:bg-slate-900 dark:text-slate-200 rounded h-10 outline-none bg-transparent focus:border-amber-500/50 dark:focus:border-amber-500/50 focus:shadow-none focus:ring-0 text-[15px]" placeholder="Email :"/>
+                                    <input name="email" id="email" type="email" className="form-input w-full py-2 px-3 border border-inherit dark:border-gray-800 dark:bg-slate-900 dark:text-slate-200 rounded h-10 outline-none bg-transparent focus:border-amber-500/50 dark:focus:border-amber-500/50 focus:shadow-none focus:ring-0 text-[15px]" placeholder="Email :" onChange={(e) => setEmail(e.target.value)} value={email} required/>
                                 </div>
                             </div>
 
                             <div className="grid grid-cols-1">
                                 <div className="mb-5">
-                                    <input name="subject" id="subject" className="form-input w-full py-2 px-3 border border-inherit dark:border-gray-800 dark:bg-slate-900 dark:text-slate-200 rounded h-10 outline-none bg-transparent focus:border-amber-500/50 dark:focus:border-amber-500/50 focus:shadow-none focus:ring-0 text-[15px]" placeholder="Subject :"/>
+                                    <input name="subject" id="subject" className="form-input w-full py-2 px-3 border border-inherit dark:border-gray-800 dark:bg-slate-900 dark:text-slate-200 rounded h-10 outline-none bg-transparent focus:border-amber-500/50 dark:focus:border-amber-500/50 focus:shadow-none focus:ring-0 text-[15px]" placeholder="Subject :" onChange={(e) => setSubject(e.target.value)} value={subject} required/>
                                 </div>
 
                                 <div className="mb-5">
-                                    <textarea name="comments" id="comments" className="form-input w-full py-2 px-3 border border-inherit dark:border-gray-800 dark:bg-slate-900 dark:text-slate-200 rounded h-28 outline-none bg-transparent focus:border-amber-500/50 dark:focus:border-amber-500/50 focus:shadow-none focus:ring-0 text-[15px]" placeholder="Message :"></textarea>
+                                    <textarea name="comments" id="comments" className="form-input w-full py-2 px-3 border border-inherit dark:border-gray-800 dark:bg-slate-900 dark:text-slate-200 rounded h-28 outline-none bg-transparent focus:border-amber-500/50 dark:focus:border-amber-500/50 focus:shadow-none focus:ring-0 text-[15px]" placeholder="Message :" onChange={(e) => setMessage(e.target.value)} value={message} required></textarea>
                                 </div>
                             </div>
                             <button type="submit" id="submit" name="send" className="btn bg-amber-500 hover:bg-amber-600 border-amber-500 hover:border-amber-600 text-white rounded-md h-11 justify-center flex items-center">Send Message</button>
